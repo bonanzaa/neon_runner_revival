efea122e5521dbb6c3a3ab6b68b19ccf9dfb4ac5
Pete <pete.kolpakov@gmail.com>
2021-06-29 17:16:02 +0200

    updated and added player marker scripts

---
+++ NeonRunnerRevival/Assets/Scripts/PlayerMarker.cs
@@ -0,0 +1,23 @@
+﻿using NeonRunnerRevival.Assets.Scripts.Manager;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NeonRunnerRevival.Assets.Scripts
+{
+    class PlayerMarker : MonoBehaviour
+    {
+        private PlayerTracker _tracker;
+        private void Awake()
+        {
+            ForceGameManagerToKnowAboutMe();
+        }
+
+        private void ForceGameManagerToKnowAboutMe()
+        {
+            _tracker = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PlayerTracker>();
+            _tracker.CashPlayerReference(this);
+        }
+    }
+}
